41aecede9ca62fcceec5b3e84373ca5fd1e0b69c
rahulry26 <ry33420@gmail.com>
2025-05-11 10:32:03 +0530

    Make start.sh executable

--- start.sh
+++ start.sh
@@ -1,9 +1,11 @@
 
 #!/bin/bash
 
-# Default to 8000 if PORT is not set
-PORT=${PORT:-8000}
+# Default to port 10000 if not set
+PORT=${PORT:-10000}
 
-echo "Starting server on port $PORT"
+echo "Starting AutoHeal.AI on port $PORT"
+
+# Start Uvicorn using dynamic port
 uvicorn app.main:app --host 0.0.0.0 --port $PORT
 
