b54bef1a7534650a84ce7bef5017e44d25413159
rahulry26 <ry33420@gmail.com>
2025-05-11 08:55:59 +0530

    Updated FastAPI app to bind with Render port env

--- app/main.py
+++ app/main.py
@@ -1,14 +1,22 @@
 
-# app/main.py
+# main.py
 
 from fastapi import FastAPI
-from app.schemas import LogRequest, FixResponse
+from pydantic import BaseModel
+from typing import List
 from utils.search_fix import find_similar_logs_and_suggest_fix
 
 app = FastAPI(
     title="AutoHeal.AI - Intelligent Auto Healing API",
     version="1.0.0"
 )
+
+class LogRequest(BaseModel):
+    log_text: str
+
+class FixResponse(BaseModel):
+    similar_logs: List[str]
+    suggested_fix: str
 
 @app.post("/heal-log", response_model=FixResponse)
 def heal_log(request: LogRequest):
